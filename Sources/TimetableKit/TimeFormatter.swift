0eb3ae602bb87f12cfffdce76feb2ade258378af
Simon Gaus <phisto05@gmail.com>
2020-07-10 14:18:45 +0200

    XCODE

---
+++ Sources/TimetableKit/TimeFormatter.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Simon Gaus on 10.07.20.
+//
+
+import Foundation
+
+class TimeFormatter {
+    
+    func string(from date: Date) -> String {
+        return ""
+    }
+    
+    func string(from interval: DateInterval) -> String {
+        return ""
+    }
+    
+    func descriptionOfRelativePositionToNow(and interval: DateInterval) -> String {
+        return ""
+    }
+    
+    /*
+    lazy var calendar: NSCalendar = {
+        var cal = NSCalendar.current as NSCalendarcal
+        cal.timeZone = TimeZone.init(identifier: "Europe/Berlin")!
+        return cal
+    }()
+    */
+    
+}
