55bfebef97d4377ed127826ce66f4e41fa9ff3f8
Simon Gaus <phisto05@gmail.com>
2020-09-24 01:25:54 +0200

    Initialize example app

---
+++ ExampleApp/TimetableKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimetableKitApp.swift
+//  TimetableKit
+//
+//  Created by Simon Gaus on 24.09.20.
+//
+
+import SwiftUI
+
+@main
+struct TimetableKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
