4c162c376d53e0bf9c8d8a2aa5c2b993dc301de9
Simon Gaus <phisto05@gmail.com>
2020-09-27 01:07:51 +0200

    Implement TimeFormatter

--- Sources/TimetableKit/Misc/TimeFormatter.swift
+++ Sources/TimetableKit/Misc/TimeFormatter.swift
@@ -5,28 +5,90 @@
 //  Created by Simon Gaus on 10.07.20.
 //
 
-import Foundation
+import UIKit
 
 class TimeFormatter {
     
-    func string(from date: Date) -> String {
-        return ""
-    }
-    
-    func string(from interval: DateInterval) -> String {
-        return ""
-    }
-    
-    func descriptionOfRelativePositionToNow(and interval: DateInterval) -> String {
-        return ""
-    }
-    
-    /*
-    lazy var calendar: NSCalendar = {
-        var cal = NSCalendar.current as NSCalendarcal
+    lazy private var calendar: Calendar = {
+        var cal = Calendar.current
         cal.timeZone = TimeZone.init(identifier: "Europe/Berlin")!
         return cal
     }()
-    */
     
+    lazy private var dateFormatter: DateFormatter = {
+        var dateFormatter = DateFormatter.init()
+        dateFormatter.dateFormat = "HH:mm"
+        return dateFormatter
+    }()
+    
+    lazy private var componentsFormatter: DateComponentsFormatter = {
+        var formatter = DateComponentsFormatter.init()
+        formatter.unitsStyle = .full
+        formatter.calendar = self.calendar
+        return formatter
+    }()
+    
+    func string(from date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+    func string(from interval: DateInterval) -> String {
+        return "\(dateFormatter.string(from: interval.start)) - \(dateFormatter.string(from: interval.end))"
+    }
+    
+    func descriptionOfRelativePositionToNow(and interval: DateInterval) -> String {
+        
+        var value = ""
+        let timeTillShowStart = interval.start.timeIntervalSince(currentDate())
+        
+        // current time is before the start of the event
+        if timeTillShowStart >= 0 {
+            let format = NSLocalizedString("Noch %@", comment: "UI String - TimeFormatter - String describing how much time is left before an event starts.")
+            value = String.init(format: format, reasonableTimeString(from: timeTillShowStart))
+        }
+        // current time is after the start of the event
+        else {
+            let timeTillShowEnd = interval.end.timeIntervalSince(currentDate())
+            if timeTillShowEnd >= 0 {
+                let format = NSLocalizedString("Läuft seit %@", comment: "UI String - TimeFormatter - String describing how much time is left before an event ends.")
+                value = String.init(format: format, reasonableTimeString(from: timeTillShowEnd))
+            }
+            else {
+                let format = NSLocalizedString("Seit %@ vorbei", comment: "UI String - TimeFormatter - String describing how much time has passed since an event ended.")
+                value = String.init(format: format, reasonableTimeString(from: timeTillShowEnd))
+            }
+        }
+        return value
+    }
+
+    private func reasonableTimeString(from duration: TimeInterval) -> String {
+        
+        var value = ""
+        let hours = Int(duration)/3600
+        let minutes = Int(duration) % 3600 / 60
+        
+        if hours >= 24 {
+            value = NSLocalizedString("mehr als 24 Stunden", comment: "UI String - TimeFormatter - String describing that an event is (or was) more than 24 hours from now.")
+        }
+        else if hours > 4 {
+            var components = DateComponents.init()
+            components.hour = hours
+            let hoursString = componentsFormatter.string(from: components) ?? "<invalid>"
+            let format = NSLocalizedString("ca. %@", comment: "UI String - TimeFormatter - String describing an duration of approximately '%@' hours.")
+            value = String.init(format: format, hoursString)
+        }
+        else {
+            var components = DateComponents.init()
+            components.hour = hours
+            components.minute = minutes
+            value = componentsFormatter.string(from: components) ?? "<invalid>"
+        }
+        
+        return value
+    }
+    
+    #warning("make this a delegate function")
+    private func currentDate() -> Date {
+        return Date.init()
+    }
 }
