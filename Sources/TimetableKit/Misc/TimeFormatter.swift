a0572adadd3d17a7be9ab3646b080ed6f6b3b0f8
Simon Gaus <phisto05@gmail.com>
2021-02-09 04:56:27 +0100

    Add convenient function

--- Sources/TimetableKit/Misc/TimeFormatter.swift
+++ Sources/TimetableKit/Misc/TimeFormatter.swift
@@ -36,6 +36,31 @@ class TimeFormatter {
         return "\(dateFormatter.string(from: interval.start)) - \(dateFormatter.string(from: interval.end))"
     }
     
+    func descriptionOfRelativePosition(of interval: DateInterval, to date: Date) -> String {
+        
+        var value = ""
+        let timeTillShowStart = interval.start.timeIntervalSince(date)
+        
+        // current time is before the start of the event
+        if timeTillShowStart >= 0 {
+            let format = NSLocalizedString("Noch %@", bundle: .module, comment: "UI String - TimeFormatter - String describing how much time is left before an event starts.")
+            value = String.init(format: format, reasonableTimeString(from: timeTillShowStart))
+        }
+        // current time is after the start of the event
+        else {
+            let timeTillShowEnd = interval.end.timeIntervalSince(currentDate())
+            if timeTillShowEnd >= 0 {
+                let format = NSLocalizedString("Läuft seit %@", bundle: .module, comment: "UI String - TimeFormatter - String describing how much time is left before an event ends.")
+                value = String.init(format: format, reasonableTimeString(from: timeTillShowEnd))
+            }
+            else {
+                let format = NSLocalizedString("Seit %@ vorbei", bundle: .module, comment: "UI String - TimeFormatter - String describing how much time has passed since an event ended.")
+                value = String.init(format: format, reasonableTimeString(from: timeTillShowEnd))
+            }
+        }
+        return value
+    }
+    
     func descriptionOfRelativePositionToNow(and interval: DateInterval) -> String {
         
         var value = ""
