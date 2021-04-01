8a6e669f7f36fc61427526e62e0443b16ffc8a61
Simon Gaus <phisto05@gmail.com>
2021-04-01 17:53:09 +0200

    Add localisation and code documentation

--- Sources/TimetableKit/Misc/TimeFormatter.swift
+++ Sources/TimetableKit/Misc/TimeFormatter.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-public class TimeFormatter: TimetableClock {
+
+/// The time formatter can be used to create representations of dates, times and of dates and times relative to each other.
+public class TimeFormatter {
     
     public init() { }
     
     lazy private var calendar: Calendar = {
         var cal = Calendar.current
-        cal.timeZone = TimeZone.init(identifier: "Europe/Berlin")!
+        cal.timeZone = TimeZone.current
         return cal
     }()
     
@@ -36,18 +38,32 @@ public class TimeFormatter: TimetableClock {
         return formatter
     }()
     
+    /// Creates a string represetation of the given date using only the time information. Format: HH:mm
+    /// - Parameter date: The date to get the string representation from.
+    /// - Returns: The description of the date.
     public func string(from date: Date) -> String {
         return houreFormatter.string(from: date)
     }
     
+    /// Creates a string represetation of the given date interval using only the time information. Format: HH:mm - HH:mm
+    /// - Parameter interval: The interval to get the string representation from.
+    /// - Returns: The description of the date interval.
     public func string(from interval: DateInterval) -> String {
         return "\(houreFormatter.string(from: interval.start)) - \(houreFormatter.string(from: interval.end))"
     }
     
+    /// Creates a string represetation of the given date using only the date information. Format: EEEE, d MMM
+    /// - Parameter date: The date to get the string representation from.
+    /// - Returns: The description of the date.
     public func dayString(from date: Date) -> String {
         return dayFormatter.string(from: date)
     }
     
+    /// Creates a description of the given date relative to the given date interval. The description is based on whether the date is before, after or in between the given date interval.
+    /// - Parameters:
+    ///   - date: The date to get the description from.
+    ///   - interval: The date interval the description is relativ to.
+    /// - Returns: The description of the date.
     public func description(of date: Date, relativeTo interval: DateInterval) -> String {
         
         var value = ""
@@ -58,7 +74,8 @@ public class TimeFormatter: TimetableClock {
             let timeTillShowEnd = interval.end.timeIntervalSince(date)
             // interval.end is earlier than date the show is over!
             if timeTillShowEnd < 0 {
-                value = String.init(format: "Seit %@ vorbei", reasonableTimeString(from: timeTillShowEnd))
+                let formatString = NSLocalizedString("Over since %@", comment: "UI String - String describing how long the date is after the time interval.")
+                value = String.init(format: formatString, reasonableTimeString(from: timeTillShowEnd))
             }
             // the show is still running ...
             else {
@@ -66,11 +83,13 @@ public class TimeFormatter: TimetableClock {
                 let timePlayed = lengthOfShow-timeTillShowEnd
                 // there is more than half of the event left
                 if timePlayed < lengthOfShow/1.8 {
-                    value = String.init(format: "Läuft seit %@", reasonableTimeString(from: timePlayed))
+                    let formatString = NSLocalizedString("Running since %@", comment: "UI String - String describing how long the date is after the start of the time interval.")
+                    value = String.init(format: formatString, reasonableTimeString(from: timePlayed))
                 }
                 // There is less than half of the event left
                 else {
-                    value = String.init(format: "Läuft noch %@", reasonableTimeString(from: timeTillShowEnd))
+                    let formatString = NSLocalizedString("Approx. %@ left", comment: "UI String - String describing how much time is left until the end of the the time interval.")
+                    value = String.init(format: formatString, reasonableTimeString(from: timeTillShowEnd, false))
                 }
             }
         }
@@ -80,21 +99,24 @@ public class TimeFormatter: TimetableClock {
             timeTillShowStart = abs(timeTillShowStart)
             
             if timeTillShowStart.minutes < 1.0 {
-                value = "Jetzt!"
+                value = NSLocalizedString("Now!", comment: "UI String - String describing NOW!.")
             }
             else {
-                value = String.init(format: "Noch %@", reasonableTimeString(from: timeTillShowStart))
+                let formatString = NSLocalizedString("%@ left", comment: "UI String - String describing how much time is left till the start of the time interval.")
+                value = String.init(format: formatString, reasonableTimeString(from: timeTillShowStart))
             }
         }
         
-        
-        
-    
-        
         return value
     }
-
-    private func reasonableTimeString(from duration: TimeInterval) -> String {
+    
+    /// Creates a string representation of the given duration. Reasonable means in this case that the function
+    /// will try to create descriptions that feel reasonable for the domain of events.
+    /// - Parameters:
+    ///   - duration: The duration to describe.
+    ///   - approx: Boolean value indicating if approximately should be included in the description.
+    /// - Returns: The description of the duration.
+    private func reasonableTimeString(from duration: TimeInterval, _ approx: Bool = true) -> String {
 
         let absoluteDuration = abs(duration)
                 
@@ -102,16 +124,20 @@ public class TimeFormatter: TimetableClock {
         let hours = Int(absoluteDuration) / 3600
         let minutes = Int(absoluteDuration) % 3600 / 60
         
-        //print("hours: \(hours) minutes: \(minutes)")
-        
         if hours >= 24 {
-            value = "mehr als 24 Stunden"
+            value = NSLocalizedString("more than 24 hours", comment: "UI String - More than 24 hours.")
         }
         else if hours > 4 {
             var components = DateComponents.init()
             components.hour = hours
             let hoursString = componentsFormatter.string(from: components) ?? "<invalid>"
-            value = String.init(format: "ca. %@", hoursString)
+            if approx {
+                let formatString = NSLocalizedString("approx. %@", comment: "UI String - Approximately <time>")
+                value = String.init(format: formatString, hoursString)
+            }
+            else {
+                value = String.init(format: "%@", hoursString)
+            }
         }
         else {
             var components = DateComponents.init()
@@ -121,9 +147,5 @@ public class TimeFormatter: TimetableClock {
         }
         
         return value
-    }
-    
-    public func currentDate(_ timetableView: TimetableView) -> Date {
-        return timetableView.clock?.currentDate(timetableView) ?? Date()
     }
 }
