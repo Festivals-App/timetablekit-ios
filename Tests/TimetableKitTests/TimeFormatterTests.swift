cc4c034d3c18fa279b2903ecc3c6841bcace67e7
Simon Gaus <phisto05@gmail.com>
2021-04-03 03:29:20 +0200

    Add tests

--- Tests/TimetableKitTests/TimeFormatterTests.swift
+++ Tests/TimetableKitTests/TimeFormatterTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import TimetableKit
 
 class TimeFormatterTests: XCTestCase {
 
@@ -16,17 +17,44 @@ class TimeFormatterTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
+    
+    func testStringFromDate() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let formatter = TimeFormatter()
+        let date = Date(timeIntervalSince1970: 1617410987)
+        XCTAssertEqual(formatter.string(from: date), "02:49")
+    }
+    
+    func testStringFromInterval() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let formatter = TimeFormatter()
+        let start = Date(timeIntervalSince1970: 1617410987)
+        XCTAssertEqual(formatter.string(from: DateInterval(start: start, duration: 60*60)), "02:49 - 03:49")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testDayStringFromDate() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let formatter = TimeFormatter()
+        let date = Date(timeIntervalSince1970: 1617410987)
+        XCTAssertEqual(formatter.dayString(from: date), "Samstag, 3 Apr.")
     }
 
+    func testDescriptionOfDate() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let formatter = TimeFormatter()
+        let date = Date(timeIntervalSince1970: 1617410987)
+        let start = Date(timeIntervalSince1970: 1617410987)
+        let interval = DateInterval(start: start, duration: 60*60)
+        
+        XCTAssertEqual(formatter.description(of: date.advanced(by:-(60*5)), relativeTo:interval), "Noch 5 Minuten")
+        XCTAssertEqual(formatter.description(of: date, relativeTo:interval), "Jetzt!")
+        XCTAssertEqual(formatter.description(of: date.advanced(by:(60*5)), relativeTo:interval), "Läuft seit 5 Minuten")
+        XCTAssertEqual(formatter.description(of: date.advanced(by:((60*60)-(60*5))), relativeTo:interval), "Läuft noch ca. 5 Minuten")
+        XCTAssertEqual(formatter.description(of: date.advanced(by:60*60), relativeTo:interval), "Vorbei")
+        XCTAssertEqual(formatter.description(of: date.advanced(by:((60*60)+(60*5))), relativeTo:interval), "Seit 5 Minuten vorbei")
+    }
 }
